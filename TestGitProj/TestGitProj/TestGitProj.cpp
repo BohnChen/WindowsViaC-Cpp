ef712c0bf210fb268b2e967c3e53803374ad5591
Bohn <arnold.bohan@gmail.com>
2022-06-28 09:44:18 +0800

    test测试一下vs中的git

--- TestGitProj/TestGitProj/TestGitProj.cpp
+++ TestGitProj/TestGitProj/TestGitProj.cpp
@@ -1,20 +1,27 @@
 ﻿// TestGitProj.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
-
+//extern "C" const IMAGE_DOS_HEADER __ImageBase;
 #include <iostream>
+#include <windows.h>
+#include <WinError.h>
+#include <stdio.h>
+#include <wchar.h>
+//#include <StdIo.h>
+using namespace std;
 
-int main()
+//int _tmain(int argc, TCHAR* argv[], TCHAR* env[]);
+
+//void DumpModule()
+//{
+//	HMODULE  hModule = GetModuleHandle(NULL);
+//	_tprintf(TEXT("with __ImageBase  = 0x%x\r\n"), (HINSTANCE)&__ImageBase);
+//}
+
+
+//Int WINAPI _tWinMain (HINSTANCE hInstanceExe, HINSTANCE, PTSTR pszCmdLine, int nCmdShow)
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
-    std::cout << "Hello World!\n";
+	cout << " Main entrance here. " << endl;
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
